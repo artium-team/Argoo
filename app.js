9be01a8486c17f76f998a8a7f5e1c9f1dafca103
Nguyễn Gia Bách <108968549+nguyengiabach1201@users.noreply.github.com>
2023-11-07 21:15:52 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function game4(question, a, b, c, d, timer, correct) {
     globalCorrect = correct, globalTimer = timer
     document.querySelector(".body").insertAdjacentHTML("beforeend", `<div id="main" class="flex flex-col h-screen"><div class="flex-1 bg-white"><div class="flex h-full justify-center items-center"><p class="text-4xl font-bold">${question}</p></div></div><div class="flex-1 bg-white"><div class="h-full grid grid-cols-2 grid-rows-2 gap-2 border-8 border-white"><div id="a" class="col-span-1 row-span-1 bg-red-500"><div class="flex h-full justify-center items-center"><p class="text-2xl text-white font-bold">${a}</p></div></div><div id="b" class="col-span-1 row-span-1 bg-blue-500"><div class="flex h-full justify-center items-center"><p class="text-2xl text-white font-bold">${b}</p></div></div><div id="c" class="col-span-1 row-span-1 bg-green-500"><div class="flex h-full justify-center items-center"><p class="text-2xl text-white font-bold">${c}</p></div></div><div id="d" class="col-span-1 row-span-1 bg-yellow-500"><div class="flex h-full justify-center items-center"><p class="text-2xl text-white font-bold">${d}</p></div></div></div></div></div>`)
 
-    Next()
+    initNextButton()
 
     Timer(timer)
 }
@@ -27,7 +27,7 @@ function game2(question, a, b, timer, correct) {
     globalCorrect = correct, globalTimer = timer
     document.querySelector(".body").insertAdjacentHTML("beforeend", `<div id="main" class="flex flex-col h-screen"><div class="flex-1 bg-white"><div class="flex h-full justify-center items-center"><p class="text-4xl font-bold">${question}</p></div></div><div class="flex-1 bg-white"><div class="h-full grid grid-cols-2 grid-rows-1 gap-2 border-8 border-white"><div id="a" class="col-span-1 row-span-1 bg-red-500"><div class="flex h-full justify-center items-center"><p class="text-2xl text-white font-bold">${a}</p></div></div><div id="b" class="col-span-1 row-span-1 bg-blue-500"><div class="flex h-full justify-center items-center"><p class="text-2xl text-white font-bold">${b}</p></div></div>`)
 
-    Next()
+    initNextButton()
 
     Timer(timer)
 }
@@ -80,10 +80,23 @@ function Timer(timer) {
     }, 1000);
 }
 
-function Next() {
+function next() {
+    clearInterval(interval)
+    if (globalTimer > 0) {
+        globalTimer = 0
+        document.querySelector("#timer-value").innerHTML = "Đáp án!"
+        for (i = 0; i < 4; i++) {
+            if (i != globalCorrect - 1) {
+                document.querySelector(`#${answers[i]}`).style.opacity -= 0.1
+            }
+        }
+    } else change()
+}
+
+function initNextButton() {
     if (questionNumber != questionList.length - 1) {
         document.querySelector("#main").insertAdjacentHTML("beforebegin", `<div id="next" class="absolute right-0 w-40 h-20 bg-slate-100 rounded-3xl flex justify-center items-center m-2 font-semibold text-2xl"><p>Tiếp theo</p></div>`)
-        document.querySelector("#next").onclick = change;
+        document.querySelector("#next").onclick = next;
     }
 }
 
@@ -91,15 +104,6 @@ function Next() {
 document.addEventListener('keydown', (event) => {
     var name = event.key;
     if (name = ("Enter" || "ArrowDown" || "ArrowRight")) {
-        clearInterval(interval)
-        if (globalTimer > 0) {
-            globalTimer = 0
-            document.querySelector("#timer-value").innerHTML = "Đáp án!"
-            for (i = 0; i < 4; i++) {
-                if (i != globalCorrect - 1) {
-                    document.querySelector(`#${answers[i]}`).style.opacity -= 0.1
-                }
-            }
-        } else change()
+        next()
     }
 }, false);
