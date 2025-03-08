89dcad50cc11c7c9cb67ad38e39d94ee6c2ce4db
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2025-03-08 07:26:16 +0700

    Add new file `questions.js`

---
+++ src/questions.js
@@ -0,0 +1,23 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "Paris", correct: true },
+            { text: "London", correct: false },
+            { text: "Rome", correct: false },
+            { text: "Berlin", correct: false },
+        ],
+    },
+    {
+        question: "Who is the President of the USA?",
+        answers: [
+            { text: "Barack Obama", correct: false },
+            { text: "Joe Biden", correct: true },
+            { text: "Donald Trump", correct: false },
+            {
+                text: "George Bush",
+                correct: false,
+            },
+        ],
+    },
+];
--- src/script.js
+++ src/script.js
@@ -72,27 +72,3 @@ function clearStatusClass(element) {
     element.classList.remove("correct");
     element.classList.remove("wrong");
 }
-
-const questions = [
-    {
-        question: "What is the capital of France?",
-        answers: [
-            { text: "Paris", correct: true },
-            { text: "London", correct: false },
-            { text: "Rome", correct: false },
-            { text: "Berlin", correct: false },
-        ],
-    },
-    {
-        question: "Who is the President of the USA?",
-        answers: [
-            { text: "Barack Obama", correct: false },
-            { text: "Joe Biden", correct: true },
-            { text: "Donald Trump", correct: false },
-            {
-                text: "George Bush",
-                correct: false,
-            },
-        ],
-    },
-];
